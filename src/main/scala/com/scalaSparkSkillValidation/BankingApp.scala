ac0be83bce2e8333fcc182feb0919d77a9594061
Im-Gonzo <gonzaloalcala0304@gmail.com>
2024-07-11 09:38:50 -0600

    refactor: Refactor processBankingData method call for testing

--- src/main/scala/com/scalaSparkSkillValidation/BankingApp.scala
+++ src/main/scala/com/scalaSparkSkillValidation/BankingApp.scala
@@ -5,13 +5,18 @@ import org.apache.log4j._
 import BankingAppImplicits._
 
 object BankingApp {
+
+  def processBankingData(implicit spark: SparkSession): Unit = {
+    spark.processBankingData()
+  }
+
   def main(args: Array[String]): Unit = {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     implicit val spark: SparkSession = SparkSession.builder().appName("BankApp").master("local[*]").getOrCreate()
 
-    spark.processBankingData()
+    processBankingData
     spark.stop()
   }
 }
