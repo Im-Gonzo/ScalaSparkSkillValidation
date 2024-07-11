5730b0b8678c6b4cdd2dd60f2406a4d700654ccd
Im-Gonzo <gonzaloalcala0304@gmail.com>
2024-07-10 21:28:08 -0600

    feature: Add initial core project logic

--- src/main/scala/com/scalaSparkSkillValidation/BankingApp.scala
+++ src/main/scala/com/scalaSparkSkillValidation/BankingApp.scala
@@ -9,36 +9,45 @@ import com.scalaSparkSkillValidation.utils.DataGenerator
 object BankingApp {
 
   /**
+   * Creates a temporary view that joins accounts and accountHolders tables
+   *
+   * @param spark: SparkSession
+   * @return Unit: Doesn't return anything
+   *
+   * @note This function assumes `accounts` and `accountHolders` exists in the Spark SQL Catalog.
    * */
-  def createJoinedAccountsView(spark: SparkSession): DataFrame ={
+  def createJoinedAccountsView(spark: SparkSession): Unit ={
     spark.sql(
       """
         |CREATE OR REPLACE TEMPORARY VIEW joined_accounts AS
         |SELECT
         | a.AccountID,
-        | a.CreatedAt,
-        | a.CreatedDate,
+        | a.OpenTimestamp as CreatedAt,
+        | a.OpenDate as CreatedDate,
         | a.Balance,
         | ah.CustomerID,
-        | ah.HolderType,
-        | from_utc_timestamp(a.CreatedAt, 'UTC') AS CreatedAtLocal
+        | ah.Relationship as HolderType,
+        | from_utc_timestamp(a.OpenTimestamp, 'UTC') AS CreatedAtLocal
         | FROM accounts a
         | LEFT JOIN accountHolders ah ON a.AccountID = ah.AccountID
         |""".stripMargin)
   }
 
-  /***/
-  def executeQueries(spark: SparkSession): DataFrame ={
-    createJoinedAccountsView(spark)
-  }
-
-  def processBankingData(spark: SparkSession): DataFrame = {
+  /**
+   * Process banking data by creating a series of views available in the Spark SQL Catalog
+   *
+   * @param spark: SparkSession
+   * @throws Exception: Handles exceptions internally.
+   *
+   * @note This function assumes that `accounts`, `transactions`, `customers`, `accountHolders`
+   *       tables/views exists in the Spark SQL Catalog.
+   * */
+  def processBankingData(spark: SparkSession): Unit = {
       try{
-        executeQueries(spark)
+        createJoinedAccountsView(spark)
       }catch {
         case e: Exception =>
           println(s"Error processing banking data: ${e.getMessage}")
-          spark.emptyDataFrame
       }
   }
 
@@ -49,8 +58,6 @@ object BankingApp {
     val spark: SparkSession = SparkSession.builder().appName("BankApp").master("local[*]").getOrCreate()
 
     processBankingData(spark)
-    val result = processBankingData(spark)
-    result.show(truncate = false)
     spark.stop()
   }
 }
