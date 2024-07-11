1cde82fbace91fde70ebf56b2002b3381a205bb4
Im-Gonzo <gonzaloalcala0304@gmail.com>
2024-07-11 10:52:18 -0600

    feature: Implement DataFrame version

--- src/main/scala/com/scalaSparkSkillValidation/BankingApp.scala
+++ src/main/scala/com/scalaSparkSkillValidation/BankingApp.scala
@@ -1,13 +1,19 @@
 package com.scalaSparkSkillValidation
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.log4j._
 import BankingAppSQLImplicits._
+import BankingAppDataFrameImplicits._
 
 object BankingApp {
 
   def processBankingDataSQL(implicit spark: SparkSession): Unit = {
     spark.processBankingDataSQL()
+  }
+
+  def processBankingDataDataFrame(implicit spark: SparkSession): Unit = {
+    val customerOverallView: DataFrame = spark.createCustomerOverallDataFrame()
+    customerOverallView.createOrReplaceTempView("customer_overall_summary_df")
   }
 
   def main(args: Array[String]): Unit = {
@@ -17,6 +23,8 @@ object BankingApp {
     implicit val spark: SparkSession = SparkSession.builder().appName("BankApp").master("local[*]").getOrCreate()
 
     processBankingDataSQL
+    processBankingDataDataFrame
+
     spark.stop()
   }
 }
