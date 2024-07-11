4749820dee410ae16339b99a850270b545c9c554
Im-Gonzo <gonzaloalcala0304@gmail.com>
2024-07-10 21:35:03 -0600

    feature: Add ScalaDocs

--- src/test/scala/BankingAppTest.scala
+++ src/test/scala/BankingAppTest.scala
@@ -8,9 +8,15 @@ import com.holdenkarau.spark.testing.SharedSparkContext
 import com.scalaSparkSkillValidation.utils.DataGenerator
 import org.apache.spark.sql.functions._
 
-
+/**
+ * Test suite for BankingApp functionalities.
+ * */
 class BankingAppTest extends AnyFlatSpec with Matchers with SharedSparkContext {
 
+  /**
+   * Test if BakingApp processes banking dummy data correctly.
+   * Generates sample data, registers it in Spark SQL Catalog, process it, and verifies result.
+   * */
   "BankingApp" should "process banking data correctly" in {
     val spark = SparkSession.builder().config(sc.getConf).getOrCreate()
 
@@ -27,7 +33,7 @@ class BankingAppTest extends AnyFlatSpec with Matchers with SharedSparkContext {
 
     BankingApp.processBankingData(spark)
 
-    /**@note Data for testing gets pulled from Spark SQL Catalog*/
+    /**@note Table `joined_accounts` gets queried from Spark SQL Catalog*/
     val joinedAccountResult = spark.sql("SELECT * FROM joined_accounts")
 
     joinedAccountResult.count() shouldBe accounts.count()
