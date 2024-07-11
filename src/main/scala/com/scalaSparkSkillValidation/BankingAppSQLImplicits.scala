3fa6c6f9ce0795cb73551b2a2004cac5129dd0c5
Im-Gonzo <gonzaloalcala0304@gmail.com>
2024-07-11 11:18:53 -0600

    refactor: Refactor extension name

--- src/main/scala/com/scalaSparkSkillValidation/BankingAppSQLImplicits.scala
+++ src/main/scala/com/scalaSparkSkillValidation/BankingAppSQLImplicits.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 
 object BankingAppSQLImplicits {
 
-  implicit class BankingAppExtension(spark: SparkSession)
+  implicit class BankingAppSQLExtension(spark: SparkSession)
   {
 
     /**
