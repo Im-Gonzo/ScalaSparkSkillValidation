26b8bf45f0ec75031f92b7b9f59609a2707ed389
Im-Gonzo <gonzaloalcala0304@gmail.com>
2024-07-10 19:04:03 -0600

    feature: Implement inital structure of testing

---
+++ src/test/scala/BankingAppTest.scala
@@ -0,0 +1,30 @@
+package com.scalaSparkSkillValidation
+
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import com.holdenkarau.spark.testing.SharedSparkContext
+import com.scalaSparkSkillValidation.utils.DataGenerator
+import org.apache.spark.sql.functions._
+
+import java.sql.{Date, Timestamp}
+
+
+class BankingAppTest extends AnyFlatSpec with Matchers with SharedSparkContext {
+
+  "BankingApp" should "process banking data correctly" in {
+    val spark = SparkSession.builder().config(sc.getConf).getOrCreate()
+
+    import spark.implicits._
+
+    // Load dummy data
+    val (accounts, transactions, customers, accountHolders) = DataGenerator.generateSampleData(spark)
+
+    // Register DataFrames in Spark SQL Catalog.
+    accounts.createOrReplaceTempView("accounts")
+    transactions.createOrReplaceTempView("transactions")
+    customers.createOrReplaceTempView("customers")
+    accountHolders.createOrReplaceTempView("accountHolders")
+  }
+}
